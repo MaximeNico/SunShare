85dcf9ffdf5c3ea93356a363def520332ceceb4b
UrieZahard <urie.zahard@gmail.com>
2019-07-03 16:56:57 +0200

    mdn

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,52 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <div class="container-fluid app">
+      <div class="row">
+        <div class="col-3 menuSide">
+          <menuSide />
+        </div>
+        <div class="col-9 routerView">
+          <router-view/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import menuSide from '@/components/menuSide'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    menuSide
+  }
 }
 </script>
 
 <style>
+
+html, body, #app {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+}
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
+
+.container-fluid.app {
+  height: 100%;
+}
+.container-fluid.app > div {
+  height: 100%;
+}
+.container-fluid.app > div > .menuSide {
+  padding: 0;
+  max-width: 206px;
+}
+
 </style>
