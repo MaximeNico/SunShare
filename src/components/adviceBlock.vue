1a72fc2a36af4580982625285b11cabbc3f0bebb
UrieZahard <urie.zahard@gmail.com>
2019-07-04 12:19:41 +0200

    Start advice block

---
+++ src/components/adviceBlock.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="adviceBlock">
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'adviceBlock'
+}
+</script>
+
+<style lang="scss" scoped>
+
+.adviceBlock {
+    height: 150px;
+    width: 100%;
+    background-color: aliceblue;
+}
+
+</style>
