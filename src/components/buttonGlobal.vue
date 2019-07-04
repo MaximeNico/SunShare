6cf90dfb75996f23460d890b339ee3e1207e3b9a
UrieZahard <urie.zahard@gmail.com>
2019-07-04 17:01:59 +0200

    Create a Global button

---
+++ src/components/buttonGlobal.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="buttonGLobal">
+        <button type="button" :style="{ backgroundColor: bgColor, color: color, fontSize: fontSize }" class="btn btn-lg">{{ title }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'buttonGlobal',
+  props: {
+    title: {
+      type: String,
+      default: 'Découvrir'
+    },
+    color: {
+      type: String,
+      default: '#ffffff'
+    },
+    bgColor: {
+      type: String,
+      default: '#000000'
+    },
+    fontSize: {
+      type: Number,
+      default: 25
+    }
+  }
+}
+</script>
